52c080f70bc98b80266757cd39363cc687664044
samjhawkins <samjhawkins@outlook.com>
2019-07-13 21:27:15 +0100

    develop- add react routing and basic pages

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                Index and landing page
-            </div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/about" component={About}/>
+                        <Route render={() => <h1>Page not found</h1>}/>
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
